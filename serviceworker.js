ab0012f8ad9894362878938edb494c292c15d502
domepfa <onboard89@gmail.com>
2026-08-19 07:15:38 +0200

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 const CACHE = "haushaltsplan-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
